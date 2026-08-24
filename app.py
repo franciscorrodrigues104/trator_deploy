5caadae3de1897ec4a417afb69aae2f96f311dde
Francisco Ribeiro Rodrigues <francisco.rr104@gmail.com>
2026-08-24 09:50:11 +0100

    filtro_csv

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, Response
 from datetime import datetime
 import os
 from supabase import create_client, Client
